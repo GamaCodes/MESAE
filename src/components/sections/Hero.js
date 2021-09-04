c5b317bd2e36812c5f4cdf0ff255523a0c31f790
ASdeveloper-app <arturo.araujo.alvarez@gmail.com>
2021-09-03 21:15:48 -0500

    change video for image

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -3,6 +3,7 @@ import classNames from "classnames";
 import { SectionProps } from "../../utils/SectionProps";
 import ButtonGroup from "../elements/ButtonGroup";
 import Button from "../elements/Button";
+import Image from "../elements/Image";
 
 const propTypes = {
   ...SectionProps.types,
@@ -74,14 +75,13 @@ const Hero = ({
             data-reveal-value="20px"
             data-reveal-delay="800"
           >
-            <div className="responsive-video">
-              <iframe
-                title="video"
-                src="/manejo.mp4"
-                frameBorder="0"
-                allowFullScreen
-              ></iframe>
-            </div>
+            <Image
+              className="has-shadow"
+              src="https://images.pexels.com/photos/3184652/pexels-photo-3184652.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+              alt="Hero"
+              width={896}
+              height={504}
+            />
           </div>
         </div>
       </div>
